3e1b2f0bd95d08931da7a6c0f1c2f692a9921758
Asphyxia39 <42377594+Asphyxia39@users.noreply.github.com>
2020-05-28 23:41:33 +0900

    added some comments

--- events/message.js
+++ events/message.js
@@ -1,24 +1,29 @@
-const mikuReply = require('../misc/replies.js');
-module.exports = async (bot, message) => {
-    botPing = `<@${bot.user.id}>`;
-    if(message.author.bot) return;
+module.exports = (bot, message) => {
+    //Ignores other bot
+    if (message.author.bot) return;
 
-    if(message.content.startsWith(bot.config.prefix)) {
-        args = message.content.slice(bot.config.prefix).trim().split(/ +/g);
+    //Argument/command name definition.
+    botPing = "<@!" + bot.user.id + ">"; //Makes bot ping variable
+    if(message.content.startsWith(bot.config.prefix)) { //If message starts with prefix.
+        args = message.content.slice(bot.config.prefix.length).trim().split(/ +/g);
         command = args.shift().toLowerCase();
-    } else if(message.content.startsWith(botPing)) {
+    } else if(message.content.startsWith(botPing)) { //If message starts with a ping.
         args = message.content.slice(botPing.length + 1).trim().split(/ +/g);
         command = args.shift().toLowerCase();
-    } else if(message.author.id === bot.config.ownerID) {
+    } else if(message.author.id === bot.config.ownerID) { //If i'm the one sending a message.
         args = message.content.toLowerCase().trim().split(/ +/g);
-        mikuReply.send(args, message);
+        mikuReply.send(args, message); //Stupid stuff you don't need to know.
         return;
     } else {
         return;
     }
 
-    const cmd = bot.commands.get(command) || bot.commands.get(bot.aliases.get(command));
-    if(!cmd) return;
+    // Grab the command data from the bot.commands Enmap
+    const cmd = bot.commands.get(command);
 
+    // If that command doesn't exist do nothing
+    if (!cmd) return;
+
+    // Run the command
     cmd.run(bot, message, args);
 };
