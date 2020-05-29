8abb03391814e1effcdbb3e2a2baa99aae88b428
Asphyxia39 <42377594+Asphyxia39@users.noreply.github.com>
2020-05-30 02:44:17 +0900

    Created Eval Command

---
+++ commands/eval.js
@@ -0,0 +1,19 @@
+exports.run = async (bot, message, args) => {
+
+    if (message.author.id !== bot.config.ownerID) return;
+
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(bot.clean(evaled), {code:"js"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${bot.clean(err)}\n\`\`\``);
+    }
+};
+
+exports.help = {
+    name: 'eval'
+};
