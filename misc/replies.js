d598968d7380b1678221e74bca62ea3a78953148
Razgriz39 <54112532+Razgriz39@users.noreply.github.com>
2020-06-02 18:42:32 +0900

    Update replies.js

--- misc/replies.js
+++ misc/replies.js
@@ -7,7 +7,7 @@ module.exports = {
         }
         if(((args.includes('gonna'))||(args.includes('go'))||(args.includes('imma'))||(args.includes('i\'ll'))) &&
             (args.includes('sleep'))){
-            message.channel.send('Sleep well!\nHave a sweet dream!');
+            message.channel.send('Sleep well master!');
         }
         if(((args.includes('thank'))||(args.includes('thanks'))) &&
             (args.includes('miku'))){
